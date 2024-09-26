1f8cecedf63e07b61a415d304307150e9f8aec90
makiperro <jocampoj@edu.tecnocampus.cat>
2024-09-26 16:36:48 +0200

    txt reader
    added a buffered reader, probably in the wrong place though.

---
+++ Jugador.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class Jugador <E extends ItipoPieza{
+    private ArrayList<E> piezasVivas;
+
+    public Jugador(ArrayList<E> piezas){
+        piezasVivas = piezas;
+    }
+    public E buscarEnPosicion(int fil, int col) {
+        
+    }
+}
--- Torns.java
+++ Torns.java
@@ -1,29 +1,42 @@
+import java.io.IOException;
 import java.util.*;
 import java.lang.Object;
+import java.io.*;
 public class Torns <E>{
     private ArrayList<E> llistaTorns;
 
     public Torns(){
         llistaTorns = new ArrayList();
     }
-    public Torns(String nomFitxer){
-        llistaTorns = new ArrayList();
+    public Torns(String nomFitxer) throws Exception{
+        carregarDesdeFitxer(nomFitxer);
     }
 
     public void afegirTorn(E torn){
-
+    llistaTorns.add(torn);
     }
 
     public E agafarPrimerTorn(){
-        return null;//temp
+        E torn = llistaTorns.get(0);
+        llistaTorns.remove(0);
+        return torn;
     }
 
-    public void guardarAFitxer(String nomFitxer){
+    public void guardarAFitxer(String nomFitxer) throws IOException {
 
     }
 
-    private void carregarDesdeFitxer(String nomFitxer){
-
+    private void carregarDesdeFitxer(String nomFitxer) throws Exception{
+        llistaTorns = new ArrayList();
+        try(BufferedReader br = new BufferedReader(new FileReader(nomFitxer))){
+            String line;
+            while((line = br.readLine())!=null){
+            llistaTorns.add(line);
+            }
+            catch(Exception){
+                throw new Exception();
+            }
+        }
     }
 
     public void imprimirTorns(){
